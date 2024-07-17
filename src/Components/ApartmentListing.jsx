d13e1510dd93b254340d218bf4b1ed808236a738
Unchanted <omkkdwivedi@gmail.com>
2024-07-17 19:36:41 +0530

    heroindie, homebanner

--- src/Components/ApartmentListing.jsx
+++ src/Components/ApartmentListing.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
+import React from "react";
 
-const Card = ({ img, title }) => {
-    return (
-        <div className="flex items-center justify-center w-full aspect-[4/3]" style={{ backgroundImage: `url(${img})` }}>
-            <div className="p-2 border-2 hover:bg-white hover:text-black transition duration-500 hover:font-medium hover:cursor-pointer border-white text-white bg-transparent">
-                <p className='px-4 md:px-9'>{title}</p>
-            </div>
-        </div>
-    )
-}
+const Card = ({ img, title, url }) => {
+  return (
+    <div
+      className="flex items-center justify-center w-full aspect-[4/3]"
+      style={{ backgroundImage: `url(${img})` }}
+    >
+      <div className="p-2 border-2 hover:bg-white hover:text-black transition duration-500 hover:font-medium hover:cursor-pointer border-white text-white bg-transparent">
+        <a href={url} className="px-4 md:px-9">
+          {title}
+        </a>
+      </div>
+    </div>
+  );
+};
 
 const ApartmentListing = () => {
-    return (
-        <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1'>
-            <Card img="/ast8.png" title="2 BHK"/>
-            <Card img="/ast9.png" title="3 BHK"/>
-            <Card img="/ast10.png" title="Penthouse"/>
-            <Card img="/ast11.png" title="Andheri, Mum"/>
-            <Card img="/ast12.png" title="Juhu, Mum"/>
-            <Card img="/ast13.png" title="SoBo, Mum"/>
-        </div>
-    )
-}
+  return (
+    <div className="w-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 gap-1">
+      <Card img="/ast8.png" title="2 BHK" url="/search" />
+      <Card img="/ast9.png" title="3 BHK" url="/search" />
+      <Card img="/ast10.png" title="Penthouse" url="/search" />
+      <Card img="/ast11.png" title="Andheri, Mum" url="/search" />
+      <Card img="/ast12.png" title="Juhu, Mum" url="/search" />
+      <Card img="/ast13.png" title="SoBo, Mum" url="/search" />
+    </div>
+  );
+};
 
-export default ApartmentListing
+export default ApartmentListing;
--- src/Components/HeroIndie.jsx
+++ src/Components/HeroIndie.jsx
@@ -1,81 +1,78 @@
-import React from 'react'
+import React from "react";
 
 const Mobile = () => {
-    return (
-        <div className='block xl:hidden mt-1'>
-            <div className="flex flex-col w-full justify-start text-white text-center bg-[#001F66] p-4 gap-4">
-                <div className="text-4xl">
-                    <p>Astravant Realty group:</p>
-                    <p>India's #1 real estate</p>
-                    <p>agency.</p>
-                </div>
-                <img src="charman.png" className='mx-auto h-[70vh]' alt="" />
-                <div className="md:px-28">
-                    <p>
-                        Welcome to the epitome of luxury living In India-
-                        our No.1 real estate destination. Here, we redefine.
-                        opulence with our exclusive collection of
-                        properties, each crafted to perfoction with
-                        meticulous attention to detail. From breathtaking
-                        skyline views to lush green landscapes, our
-                        portfolio boasts tho pinnacle of architectural
-                        marvels. But ts not ust about the properties; is
-                        about the experience. our esteemed cliente
-                        racoivos nothing short of premior and
-                        sophisticated service, tailored to thelr every ned:
-                        Step into a realm where luxury knows no bounds,
-                        and indulge in a lifestyle beyond comparo with us
-                    </p>
-                </div>
-                <button className='border-[2px] p-2 hover:bg-white transition duration-500 hover:text-[#001F66] border-white w-1/4 mx-auto'>Learn More</button>
-            </div>
+  return (
+    <div className="block xl:hidden">
+      <div className="flex flex-col w-full justify-start text-white text-center bg-[#001F66] p-4 gap-4">
+        <div className="text-4xl">
+          <p>Astravant Realty group:</p>
+          <p>India's #1 real estate</p>
+          <p>agency.</p>
         </div>
-    
-    )
-
-}
+        <img src="charman.png" className="mx-auto h-[70vh]" alt="" />
+        <div className="md:px-28">
+          <p>
+            Welcome to the epitome of luxury living In India- our No.1 real
+            estate destination. Here, we redefine. opulence with our exclusive
+            collection of properties, each crafted to perfoction with meticulous
+            attention to detail. From breathtaking skyline views to lush green
+            landscapes, our portfolio boasts tho pinnacle of architectural
+            marvels. But ts not ust about the properties; is about the
+            experience. our esteemed cliente racoivos nothing short of premior
+            and sophisticated service, tailored to thelr every ned: Step into a
+            realm where luxury knows no bounds, and indulge in a lifestyle
+            beyond comparo with us
+          </p>
+        </div>
+        <button className="border-[2px] p-2 hover:bg-white transition duration-500 hover:text-[#001F66] border-white w-1/4 mx-auto">
+          <a href="/contact">Learn More</a>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 const Computer = () => {
-    return (
-        <div className='hidden xl:block mt-1'>
-            <div className="flex flex-row w-full justify-between text-white bg-[#001F66] h-screen">
-                <div className="flex flex-col pl-20 pb-20 pt-20 gap-10">
-                    <div className="text-5xl">
-                        <p>Espire Realty group:</p>
-                        <p>India's #1 real estate</p>
-                        <p>agency.</p>
-                    </div>
-                    <div className="w-3/5 mt-10 ">
-                        <p>Welcome to the epitome of luxury living In India-
-                            our No.1 real estate destination. Here, we redefine.
-                            opulence with our exclusive collection of
-                            properties, each crafted to perfoction with
-                            meticulous attention to detail. From breathtaking
-                            skyline views to lush green landscapes, our
-                            portfolio boasts tho pinnacle of architectural
-                            marvels. But ts not ust about the properties; is
-                            about the experience. our esteemed cliente
-                            racoivos nothing short of premior and
-                            sophisticated service, tailored to thelr every ned:
-                            Step into a realm where luxury knows no bounds,
-                            and indulge in a lifestyle beyond comparo with us
-                        </p>
-                    </div>
-                    <button className='border-[2px] p-2 hover:bg-white transition duration-500 hover:text-[#001F66] border-white w-1/4'>Learn More</button>
-                </div>
-                    <img src="charman.png" className='h-full' alt="" />
-            </div>
+  return (
+    <div className="hidden xl:block">
+      <div className="flex flex-row w-full justify-between text-white bg-[#001F66] h-screen">
+        <div className="flex flex-col pl-20 pb-20 pt-20 gap-10">
+          <div className="text-5xl">
+            <p>Espire Realty group:</p>
+            <p>India's #1 real estate</p>
+            <p>agency.</p>
+          </div>
+          <div className="w-3/5 mt-10 ">
+            <p>
+              Welcome to the epitome of luxury living In India- our No.1 real
+              estate destination. Here, we redefine. opulence with our exclusive
+              collection of properties, each crafted to perfoction with
+              meticulous attention to detail. From breathtaking skyline views to
+              lush green landscapes, our portfolio boasts tho pinnacle of
+              architectural marvels. But ts not ust about the properties; is
+              about the experience. our esteemed cliente racoivos nothing short
+              of premior and sophisticated service, tailored to thelr every ned:
+              Step into a realm where luxury knows no bounds, and indulge in a
+              lifestyle beyond comparo with us
+            </p>
+          </div>
+          <button className="border-[2px] p-2 hover:bg-white transition duration-500 hover:text-[#001F66] border-white w-1/4">
+            <a href="/contact">Learn More</a>
+          </button>
         </div>
-    )
-}
+        <img src="charman.png" className="h-full" alt="" />
+      </div>
+    </div>
+  );
+};
 
 const HeroIndie = () => {
-    return (
-        <>
-            <Computer />
-            <Mobile />
-        </>
-    )
-}
+  return (
+    <>
+      <Computer />
+      <Mobile />
+    </>
+  );
+};
 
-export default HeroIndie
+export default HeroIndie;
