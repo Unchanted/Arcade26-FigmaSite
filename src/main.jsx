b990c8030ee3fa85b250023282918161a44621e7
Unchanted <omkkdwivedi@gmail.com>
2024-08-04 17:01:30 +0530

    edits

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,9 @@ import App from './App.jsx'
 import './index.css'
 
  
-import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
       <App />
-    </ThemeProvider>
   </React.StrictMode>,
 )
